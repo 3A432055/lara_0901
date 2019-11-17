ff0ef05c40379126c9580b670a7c7de9d7e52638
CHEN <david860120@gmail.com>
2019-11-17 14:29:36 +0800

    < 練習7-1> 設定所需的 Route

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,12 @@ Route::group(['prefix' => 'admin'], function() {
 
     Route::get('posts'          , ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
     Route::get('posts/create'   , ['as' => 'admin.posts.create', 'uses' => 'AdminPostsController@create']);
+    Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'  , 'uses' => 'AdminPostsController@edit']);
     //單元練習< 練習6-4> 設定更新所需的 Route
     Route::patch('posts/{id}',    ['as' => 'admin.posts.update', 'uses' => 'AdminPostsController@update']);
     //單元練習< 練習4-3> 開啟新增的Route
     Route::post('posts',['as'=>'admin.posts.store','uses'=> 'AdminPostsController@store']);
 
-    Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'  , 'uses' => 'AdminPostsController@edit']);
+    //單元練習< 練習7-1> 設定所需的 Route
+    Route::delete('posts/{id}'  , ['as' => 'admin.posts.destroy', 'uses' => 'AdminPostsController@destroy']);
 });
