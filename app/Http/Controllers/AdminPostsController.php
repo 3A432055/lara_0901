1c3c0cdf1ee4a4cdb68c18089559d189cb913a04
CHEN <david860120@gmail.com>
2020-01-02 20:32:49 +0800

    編輯 PostsController裡設定 type­ hinting

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
-
+use App\Http\Requests\PostRequest;
 use App\Http\Requests;
 
 class AdminPostsController extends Controller
